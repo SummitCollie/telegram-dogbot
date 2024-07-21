48370f1992b59a1e306707a40c74911bd8619da3
SummitCollie <41765990+SummitCollie@users.noreply.github.com>
2024-07-21 01:09:17 -0400

    Finish first round of tests

--- spec/requests/telegram_webhooks_controller_spec.rb
+++ spec/requests/telegram_webhooks_controller_spec.rb
@@ -48,38 +48,41 @@ RSpec.describe TelegramWebhooksController, telegram_bot: :rails do
     context 'when message should be stored' do
       it 'creates User record if missing' do
         expect do
-          dispatch_message('text')
+          dispatch_message 'text'
         end.to change(User, :count).by(1)
 
         # Ensure a second message from same user doesn't create additional user record
         expect do
-          dispatch_message('text')
+          dispatch_message 'text'
         end.not_to change(User, :count)
       end
 
       it 'creates Chat record if missing' do
         expect do
-          dispatch_message('text')
+          dispatch_message 'text'
         end.to change(Chat, :count).by(1)
 
         # Ensure a second message from same chat doesn't create additional chat record
         expect do
-          dispatch_message('text')
+          dispatch_message 'text'
         end.not_to change(Chat, :count)
       end
 
       it 'stores messages from whitelisted group chats' do
-        dispatch_message('text')
+        options = default_message_options
+        dispatch_message 'text', options
 
-        user = User.find_by(api_id: default_message_options[:from][:id])
+        user = User.find_by(api_id: options[:from][:id])
         message = user.messages.last
 
         expect(message.text).to eq 'text'
+        expect(message.date.to_i).to eq options[:date]
+        expect(message.attachment_type).to be_nil
       end
 
       it 'stores caption & media type messages with media attached' do
         # Photo message
-        dispatch_message(nil, photo_message_options)
+        dispatch_message nil, photo_message_options
 
         user = User.find_by(api_id: default_message_options[:from][:id])
         message = user.messages.last
@@ -88,12 +91,30 @@ RSpec.describe TelegramWebhooksController, telegram_bot: :rails do
         expect(message.attachment_type).to eq 'photo'
 
         # Video message
-        dispatch_message(nil, video_message_options)
+        dispatch_message nil, video_message_options
 
         message = user.messages.last
 
         expect(message.text).to eq 'Video message caption text'
         expect(message.attachment_type).to eq 'video'
+      end
+
+      it 'tracks message replies' do
+        options = default_message_options
+        dispatch_message 'Original message', options
+
+        user = User.find_by(api_id: options[:from].id)
+        message1 = user.messages.last
+
+        dispatch_message 'Reply message', {
+          reply_to_message: Telegram::Bot::Types::Message.new(
+            message_id: message1.api_id
+          )
+        }
+        message2 = user.messages.last
+
+        expect(message1.reload.replies).to include(message2)
+        expect(message2.reply_to_message).to eq message1
       end
     end
 
@@ -102,7 +123,7 @@ RSpec.describe TelegramWebhooksController, telegram_bot: :rails do
         it 'updates existing Message record if found' do
           # Original message
           original_options = default_message_options
-          dispatch_message('Original text', original_options)
+          dispatch_message 'Original text', original_options
 
           user = User.find_by(api_id: original_options[:from][:id])
           message = user.messages.last
@@ -126,6 +147,26 @@ RSpec.describe TelegramWebhooksController, telegram_bot: :rails do
               text: 'Edited text'
             )
           end.not_to change(Message, :count)
+        end
+
+        it 'does not change the datetime of the original message' do
+          # Original message
+          original_options = default_message_options
+          dispatch_message 'Original text', original_options
+
+          user = User.find_by(api_id: original_options[:from][:id])
+          message = user.messages.last
+
+          expect(message.text).to eq 'Original text'
+
+          # Dispatch edit
+          dispatch edited_message: original_options.merge(
+            edit_date: 1.minute.from_now.to_i,
+            text: 'Edited text'
+          )
+
+          message.reload
+          expect(message.date.to_i).to eq original_options[:date]
         end
       end
     end
@@ -243,8 +284,49 @@ RSpec.describe TelegramWebhooksController, type: :telegram_bot_controller do
         end.to raise_error(FuckyWuckies::AuthorizationError)
       end
 
-      it 'refuses to run commands in non-group chats'
-      it 'refuses to run commands in non-whitelisted chats'
+      it 'refuses to run commands in non-group chats' do
+        allow_any_instance_of(described_class).to receive(:handle_error).and_raise(
+          FuckyWuckies::AuthorizationError
+        )
+
+        expect do
+          dispatch_command summarize: { chat: Telegram::Bot::Types::Chat.new(
+            id: 23456,
+            type: 'private',
+            title: "Someone's DMs"
+          ) }
+        end.to raise_error(FuckyWuckies::AuthorizationError)
+
+        expect do
+          dispatch_command summarize: { chat: Telegram::Bot::Types::Chat.new(
+            id: 34567,
+            type: 'channel',
+            title: 'Some channel'
+          ) }
+        end.to raise_error(FuckyWuckies::AuthorizationError)
+      end
+
+      it 'refuses to run commands in non-whitelisted chats' do
+        allow_any_instance_of(described_class).to receive(:handle_error).and_raise(
+          FuckyWuckies::AuthorizationError
+        )
+
+        expect do
+          dispatch_command summarize: { chat: Telegram::Bot::Types::Chat.new(
+            id: 23456,
+            type: 'group',
+            title: 'Some group'
+          ) }
+        end.to raise_error(FuckyWuckies::AuthorizationError)
+
+        expect do
+          dispatch_command summarize: { chat: Telegram::Bot::Types::Chat.new(
+            id: 34567,
+            type: 'supergroup',
+            title: 'Some supergroup'
+          ) }
+        end.to raise_error(FuckyWuckies::AuthorizationError)
+      end
     end
   end
 end
