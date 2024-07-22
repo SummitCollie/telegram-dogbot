09117470739869d31ffdd0163b0eccacb65ba5e6
SummitCollie <41765990+SummitCollie@users.noreply.github.com>
2024-07-22 09:57:11 -0400

    Commit before ripping out cloudflare API library

--- app/services/cloudflare_ai/summarize_chat.rb
+++ app/services/cloudflare_ai/summarize_chat.rb
@@ -12,11 +12,35 @@ module CloudflareAi
         api_token: Rails.application.credentials.cloudflare.api_token
       )
 
-      result = client.complete(
-        prompt: 'Hello, I am dog',
-        model_name: "@cf/meta/llama-3-8b-instruct",
-        max_tokens: 512
-      )
+      fake_chatlogs = File.read('/home/user/Desktop/example-chat-for-prompt.txt')
+      fake_summary = File.read('/home/user/Desktop/example-summary-for-prompt.txt')
+
+      fake_chatlogs_input = File.read('/home/user/Desktop/fake-chat-2.txt')
+      fake_chatlots_long = File.read('/home/user/Desktop/fake_chatlog_4386_tokens.txt')
+
+      messages = [
+        Cloudflare::AI::Message.new(
+          role: "system",
+          content: "You are a summarization bot tasked with summarizing the chat log provided below. " \
+                    "Your goal is to concisely highlight each chat member's stories and the general " \
+                    "subjects discussed in the chat. Do not provide opinions or suggestions. " \
+                    "Simply extract and present the key points and main themes.\n\n" \
+                    "---EXAMPLE CHAT---\n#{fake_chatlogs}\n\n" \
+                    "---EXAMPLE RESPONSE---\n#{fake_summary}"),
+        # Cloudflare::AI::Message.new(role: "user", content: fake_chatlogs),
+        # Cloudflare::AI::Message.new(role: "assistant", content: fake_summary),
+        Cloudflare::AI::Message.new(role: "user", content: fake_chatlots_long)
+      ]
+
+      result = client.chat(messages: messages, model_name: "@cf/meta/llama-3-8b-instruct", max_tokens: 512) do |data|
+        puts data
+      end
+
+      # result = client.complete(
+      #   prompt: 'Hello, I am dog',
+      #   model_name: "@cf/meta/llama-3-8b-instruct",
+      #   max_tokens: 512
+      # )
 
       #<Cloudflare::AI::Results::TextGeneration:0x00007fe3bb530e70
       # @result_data=
@@ -24,6 +48,5 @@ module CloudflareAi
       #   "success"=>true,
       #   "errors"=>[],
       #   "messages"=>[]}>
-    end
   end
 end
