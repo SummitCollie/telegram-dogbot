d3ef813f958a91b5e62b1594460d57fb938fafa3
SummitCollie <41765990+SummitCollie@users.noreply.github.com>
2024-07-27 21:32:10 -0400

    Set bot commands on startup

--- config/initializers/register_telegram_bot.rb
+++ config/initializers/register_telegram_bot.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
+# Set slash commands available from telegram UI
+# https://core.telegram.org/bots/features#commands
+Telegram.bot.set_my_commands commands: [
+  { command: 'summarize',
+    description: 'Summarize messages since last summary (or as many as possible)' },
+  { command: 'summarize_nicely', description: 'Summarize more positively' },
+  { command: 'vibe_check', description: 'Run vibe analysis' }
+]
+
+# Register endpoint for telegram webhook events in production
 Rake::Task['telegram:bot:set_webhook'].invoke if Rails.env.production?
